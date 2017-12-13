df17fc1623b5035949630d455090e2a66f4c5cf1
居凯 <jk@ifourthwall.com>
2017-12-13 17:52:14 +0800

    Initial Commit

---
+++ JKImageTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKImageTool
+//
+//  Created by 陶晨麟 on 2017/12/13.
+//  Copyright © 2017年 Fourthwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKImageTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKImageTool
+//
+//  Created by 陶晨麟 on 2017/12/13.
+//  Copyright © 2017年 Fourthwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
